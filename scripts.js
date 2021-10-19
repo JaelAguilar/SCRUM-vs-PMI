4cb1b87e36659890b4d1371d14ea89887c96d609
Jael Aguilar <cjaelaguilar@gmail.com>
2021-10-19 11:09:04 -0500

    The modal doesn't close when the page is reloaded

--- scripts.js
+++ scripts.js
@@ -5,11 +5,55 @@ var popoverTriggerList = [].slice.call(
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+//Fin de Javascript para popover
+
+//Definición de los elementos
+//Botón de continuar
+var continueButton = document.getElementById("Continue-Question")
+//Los botones de respuestas
+var answers = [...document.getElementsByClassName("respuesta")]
+//Modal de preguntas
+var questionsModal = new bootstrap.Modal(document.getElementById("questionsModal"))
+
+
+/**
+ *Abre el Modal y especifica que el modal está abierto
+ *
+ */
+function loadModal(modal) {
+    modal.show();
+    console.log('Modal Open')
+    localStorage.setItem('isModalOpen', true);
+}
+
+/**
+ *Cierra el Modal y especifica que el modal está cerrado
+ *
+ */
+function closeModal(modal) {
+    modal.hide();
+    localStorage.setItem('isModalOpen', false);
+    console.log("Modal Closed")
+}
+
+/**
+ * Cierra específicamente el modal de preguntas
+ */
+function closeQuestionModal() {
+    closeModal(questionsModal);
+}
+
+// Este código es para asegurarse que el modal se quede abierto aunque se recargue la página
+let reload = sessionStorage.getItem('pageReloaded');
+if (reload) {
+    if (localStorage.getItem('isModalOpen')==='true') {
+        loadModal(questionModal);
+    }
+}
+sessionStorage.setItem('pageReloaded', 'true');
 
 
 
-let continueButton = document.getElementById("Continue-Question");
-let answers = [...document.getElementsByClassName("respuesta")]
 
 //The pseudoclass when an element is choosed is :focus
 //Esto no funciona, aún
